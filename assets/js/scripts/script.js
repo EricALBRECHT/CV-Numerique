211846e8f708435ccacd694a945e967d6ddcd413
Eric <e.albrecht@laposte.net>
2020-02-23 22:59:41 +0100

    maj mooc

---
+++ assets/js/scripts/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    // declenche les evenements (animations) au moment ou on scroll dessus
+    var s = skrollr.init();
+    //animation fenetres
+    AOS.init({
+        duration: 1200,
+    });
+    // -------------------------------------------------------------
+    // Progress Bar
+    // -------------------------------------------------------------
+
+    $('#skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+
+        if (visible) {
+            $.each($('.progress-bar'), function() {
+                $(this).css('width', $(this).attr('aria-valuenow') + '%');
+            });
+
+            $(this).unbind('inview');
+
+        }
+
+        // -------------------------------------------------------------
+        // More skill
+        // -------------------------------------------------------------
+        $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                percent = jQuery(this.el).attr('data-percent');
+                $('.chart').easyPieChart({
+
+                    //your configuration goes here
+                    easing: 'easeOut',
+                    delay: 3000,
+                    barColor: '#00B0BD',
+                    trackColor: 'rgba(255,255,255,0.1)',
+                    scaleColor: false,
+                    lineWidth: 10,
+                    size: 140,
+                    animate: 2000,
+                    // animate the numbers
+                    onStep: function(value) {
+                        this.$el.find('span.percent').text(Math.round(value));
+                    },
+                    onStop: function(value, to) {
+                        this.$el.find('span.percent').text(Math.round(to));
+                    }
+
+                });
+                $(this).unbind('inview');
+            }
+        });
+    });
+});
